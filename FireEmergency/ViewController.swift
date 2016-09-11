0900e99a3e188d55f6396af81ad20f3b975fb322
tadakazu <tadakazu1972@gmail.com>
2016-09-11 12:45:44 +0900

    Initial Commit

---
+++ FireEmergency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireEmergency
+//
+//  Created by 中道忠和 on 2016/09/11.
+//  Copyright © 2016年 tadakazu nakamichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
