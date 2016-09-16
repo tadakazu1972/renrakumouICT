d2cbcbf3adfd5155a194cea6212383d489ff5d7d
tadakazu <tadakazu1972@gmail.com>
2016-09-17 00:19:21 +0900

    first commit

---
+++ FireEmergency/DataViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DataViewController.swift
+//  FireEmergency
+//
+//  Created by 中道忠和 on 2016/09/11.
+//  Copyright © 2016年 tadakazu nakamichi. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //Button生成
+        let btnEarthquake = UIButton()
+        btnEarthquake.frame = CGRectMake(0,0,100,40)
+        btnEarthquake.backgroundColor = UIColor.lightGrayColor()
+        btnEarthquake.layer.masksToBounds = true
+        btnEarthquake.setTitle("震災", forState: UIControlState.Normal)
+        btnEarthquake.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        btnEarthquake.setTitleColor(UIColor.redColor(), forState: UIControlState.Highlighted)
+        btnEarthquake.layer.cornerRadius = 8.0
+        btnEarthquake.layer.position = CGPoint(x:100, y:self.view.frame.height-40)
+        btnEarthquake.tag = 1
+        btnEarthquake.addTarget(self, action:#selector(DataViewController.onClickbtnEarthquake(_:)), forControlEvents: .TouchUpInside)
+        self.view.addSubview(btnEarthquake)
+    }
+    
+    //震災
+    func onClickbtnEarthquake(sender:UIButton){
+        //Viewを閉じる
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FireEmergency/ViewController.swift
+++ FireEmergency/ViewController.swift
@@ -9,11 +9,137 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let btnData         = UIButton(frame: CGRectZero)
+    let btnEarthquake   = UIButton(frame: CGRectZero)
+    let btnTyphoon      = UIButton(frame: CGRectZero)
+    let btnKokuminhogo  = UIButton(frame: CGRectZero)
+    let btnKinentai     = UIButton(frame: CGRectZero)
+    let pad1            = UIView(frame: CGRectZero) //ボタンの間にはさむ見えないpaddingがわり
+    let pad2            = UIView(frame: CGRectZero)
+    let pad3            = UIView(frame: CGRectZero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Button生成
+        //基礎データ入力
+        btnData.backgroundColor = UIColor.blueColor()
+        btnData.layer.masksToBounds = true
+        btnData.setTitle("基礎データ入力", forState: UIControlState.Normal)
+        btnData.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        btnData.setTitleColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
+        btnData.layer.cornerRadius = 8.0
+        btnData.tag = 0
+        btnData.addTarget(self, action: #selector(ViewController.onClickbtnData(_:)), forControlEvents: .TouchUpInside)
+        btnData.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(btnData)
+        //震災
+        btnEarthquake.backgroundColor = UIColor.lightGrayColor()
+        btnEarthquake.layer.masksToBounds = true
+        btnEarthquake.setTitle("震災", forState: UIControlState.Normal)
+        btnEarthquake.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        btnEarthquake.tag=1
+        btnEarthquake.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(btnEarthquake)
+        //風水害
+        btnTyphoon.backgroundColor = UIColor.lightGrayColor()
+        btnTyphoon.layer.masksToBounds = true
+        btnTyphoon.setTitle("風水害", forState: UIControlState.Normal)
+        btnTyphoon.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        btnTyphoon.tag=2
+        btnTyphoon.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(btnTyphoon)
+        //国民保護
+        btnKokuminhogo.backgroundColor = UIColor.lightGrayColor()
+        btnKokuminhogo.layer.masksToBounds = true
+        btnKokuminhogo.setTitle("国民保", forState: UIControlState.Normal)
+        btnKokuminhogo.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        btnKokuminhogo.tag=3
+        btnKokuminhogo.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(btnKokuminhogo)
+        //緊援隊
+        btnKinentai.backgroundColor = UIColor.lightGrayColor()
+        btnKinentai.layer.masksToBounds = true
+        btnKinentai.setTitle("緊援隊", forState: UIControlState.Normal)
+        btnKinentai.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        btnKinentai.tag=4
+        btnKinentai.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(btnKinentai)
+        //pad
+        pad1.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(pad1)
+        pad2.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(pad2)
+        pad3.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(pad3)
     }
+    
+    //制約ひな型
+    internal func Constraint(item: AnyObject, _ attr: NSLayoutAttribute, to: AnyObject?, _ attrTo: NSLayoutAttribute, constant: CGFloat = 0.0, multiplier: CGFloat = 1.0, relate: NSLayoutRelation = .Equal, priority: UILayoutPriority = UILayoutPriorityRequired) -> NSLayoutConstraint {
+        let ret = NSLayoutConstraint(
+            item:       item,
+            attribute:  attr,
+            relatedBy:  relate,
+            toItem:     to,
+            attribute:  attrTo,
+            multiplier: multiplier,
+            constant:   constant
+        )
+        ret.priority = priority
+        return ret
+    }
+    
+    override func viewDidLayoutSubviews(){
+        //制約
+        self.view.addConstraints([
+            //基礎データ入力ボタン
+            Constraint(btnData, .Top, to:self.view, .Top, constant:20),
+            Constraint(btnData, .Leading, to:self.view, .Leading, constant:8),
+            Constraint(btnData, .Trailing, to:self.view, .TrailingMargin, constant:8),
+            //震災ボタン
+            Constraint(btnEarthquake, .Top, to:btnData, .Bottom, constant:8),
+            Constraint(btnEarthquake, .Leading, to:self.view, .Leading, constant:8),
+            Constraint(btnEarthquake, .Width, to:self.view, .Width, multiplier:0.22, constant:0),
+            //pad1
+            Constraint(pad1, .Top, to:btnData, .Bottom, constant:8),
+            Constraint(pad1, .Leading, to:btnEarthquake, .Trailing, constant:0),
+            Constraint(pad1, .Width, to:self.view, .Width, multiplier:0.02, constant:0),
+            //風水害ボタン
+            Constraint(btnTyphoon, .Top, to:btnData, .Bottom, constant:8),
+            Constraint(btnTyphoon, .Leading, to:pad1, .Trailing, constant:0),
+            Constraint(btnTyphoon, .Width, to:btnEarthquake, .Width, constant:0),
+            //pad2
+            Constraint(pad2, .Top, to:btnData, .Bottom, constant:8),
+            Constraint(pad2, .Leading, to:btnTyphoon, .Trailing, constant:0),
+            Constraint(pad2, .Width, to:self.view, .Width, multiplier:0.02, constant:0),
+            //国民保護ボタン
+            Constraint(btnKokuminhogo, .Top, to:btnData, .Bottom ,constant:8),
+            Constraint(btnKokuminhogo, .Leading, to:pad2, .Trailing, constant:0),
+            Constraint(btnKokuminhogo, .Width, to:btnEarthquake, .Width, constant:0),
+            //pad3
+            Constraint(pad3, .Top, to:btnData, .Bottom, constant:8),
+            Constraint(pad3, .Leading, to:btnKokuminhogo, .Trailing, constant:0),
+            Constraint(pad3, .Width, to:self.view, .Width, multiplier:0.02, constant:0),
+            //緊援隊ボタン
+            Constraint(btnKinentai, .Top, to:btnData, .Bottom, constant:8),
+            Constraint(btnKinentai, .Leading, to:pad3, .Trailing, constant:0),
+            Constraint(btnKinentai, .Width, to:btnEarthquake, .Width, constant:0)
+        ])
+    }
+    
+    //基礎データ入力画面遷移
+    func onClickbtnData(sender : UIButton){
+        //dataViewControllerのインスタンス生成
+        let data = DataViewController()
+        
+        //navigationControllerのrootViewControllerにdataViewControllerをセット
+        let nav = UINavigationController(rootViewController: data)
+        
+        //画面遷移
+        self.presentViewController(nav, animated: true, completion: nil)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
