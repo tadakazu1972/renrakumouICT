9714a1e32bd06cfca46a7cf0b2503a838c1c7cd1
tadakazu1972 <tadakazu1972@gmail.com>
2017-02-25 16:50:22 +0900

    add すべて選択/解除ボタン

--- FireEmergency/ContactLoadDialog2.swift
+++ FireEmergency/ContactLoadDialog2.swift
@@ -18,11 +18,14 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
     var result: [[String]] = []
     private var btnClose: UIButton!
     private var btnMail: UIButton!
+    private var btnAll: UIButton!
     //データ保存用
     let userDefaults = NSUserDefaults.standardUserDefaults()
     var mainStation: String!
     var tsunamiStation: String!
     var kubun: String!
+    //選択_id保存用配列
+    var checkArray: [Bool] = []
     
     //コンストラクタ
     init(parentView: UIViewController, resultFrom: [[String]]){
@@ -33,7 +36,9 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
         table = UITableView()
         btnClose = UIButton()
         btnMail = UIButton()
+        btnAll = UIButton()
         result = resultFrom
+        checkArray = Array(count: result.count, repeatedValue: false) //抽出件数だけ初期化
     }
     
     //デコンストラクタ
@@ -44,6 +49,8 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
         table = nil
         btnClose = nil
         btnMail = nil
+        btnAll = nil
+        checkArray.removeAll()
     }
     
     //表示
@@ -79,7 +86,7 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
         table.dataSource = self
         table.estimatedRowHeight = 60 //下とあわせこの２行で複数表示されるときの間がひらくように
         table.rowHeight = UITableViewAutomaticDimension
-        table.registerClass(ContactCell1.self, forCellReuseIdentifier:"contactCell1")
+        table.registerClass(ContactCellCheckbox.self, forCellReuseIdentifier:"contactCellCheckbox")
         table.separatorColor = UIColor.clearColor()
         table.allowsMultipleSelection = true
         self.win1.addSubview(table)
@@ -88,26 +95,39 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
 
         
         //閉じるボタン生成
-        btnClose.frame = CGRectMake(0,0,100,30)
+        btnClose.frame = CGRectMake(0,0,80,30)
         btnClose.backgroundColor = UIColor.orangeColor()
         btnClose.setTitle("閉じる", forState: .Normal)
         btnClose.setTitleColor(UIColor.whiteColor(), forState: .Normal)
         btnClose.layer.masksToBounds = true
         btnClose.layer.cornerRadius = 10.0
-        btnClose.layer.position = CGPointMake(self.win1.frame.width/2-60, self.win1.frame.height-20)
+        btnClose.layer.position = CGPointMake(self.win1.frame.width/2, self.win1.frame.height-20)
         btnClose.addTarget(self, action: #selector(self.onClickClose(_:)), forControlEvents: .TouchUpInside)
         self.win1.addSubview(btnClose)
         
         //メール送信ボタン生成
-        btnMail.frame = CGRectMake(0,0,100,30)
+        btnMail.frame = CGRectMake(0,0,90,30)
         btnMail.backgroundColor = UIColor.redColor()
         btnMail.setTitle("メール送信", forState: .Normal)
+        btnMail.titleLabel?.font = UIFont.systemFontOfSize(14)
         btnMail.setTitleColor(UIColor.whiteColor(), forState: .Normal)
         btnMail.layer.masksToBounds = true
         btnMail.layer.cornerRadius = 10.0
-        btnMail.layer.position = CGPointMake(self.win1.frame.width/2+60, self.win1.frame.height-20)
+        btnMail.layer.position = CGPointMake(self.win1.frame.width/2+100, self.win1.frame.height-20)
         btnMail.addTarget(self, action: #selector(self.onClickMail(_:)), forControlEvents: .TouchUpInside)
         self.win1.addSubview(btnMail)
+        
+        //すべて選択/解除ボタン生成
+        btnAll.frame = CGRectMake(0,0,90,30)
+        btnAll.backgroundColor = UIColor.magentaColor()
+        btnAll.setTitle("全て選択/解除", forState: .Normal)
+        btnAll.titleLabel?.font = UIFont.systemFontOfSize(14)
+        btnAll.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        btnAll.layer.masksToBounds = true
+        btnAll.layer.cornerRadius = 10.0
+        btnAll.layer.position = CGPointMake(self.win1.frame.width/2-100, self.win1.frame.height-20)
+        btnAll.addTarget(self, action: #selector(self.onClickAll(_:)), forControlEvents: .TouchUpInside)
+        self.win1.addSubview(btnAll)
     }
     
     //閉じる
@@ -128,10 +148,12 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
         var addressArray: [String] = []
         let line = result.count //countで２次元配列の行数をカウントしてくれる！なんてこった！
         for index in 0..<line {
-            addressArray.append(result[index][2])
+            //チェック配列を上から順に見ていって、チェックされていたら同じインデックスのmailを捕獲
+            if self.checkArray[index]{
+                addressArray.append(result[index][2])
+            }
         }
-        
-        print(addressArray)
+        print(addressArray) //デバッグ用
         
         //次の関数でMailViewControllerを生成して画面遷移する
         sendMail(addressArray)
@@ -150,6 +172,15 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
         parent.presentViewController(nav, animated: true, completion: nil)
     }
     
+    //全て選択/解除
+    func onClickAll(sender: UIButton){
+        let line = result.count
+        for index in 0..<line {
+            self.checkArray[index] = !self.checkArray[index]
+        }
+        table.reloadData()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection sction:Int)-> Int {
         return self.result.count
     }
@@ -159,8 +190,9 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCell {
-        let cell:ContactCell1 = table.dequeueReusableCellWithIdentifier("contactCell1")! as! ContactCell1
+        let cell:ContactCellCheckbox = table.dequeueReusableCellWithIdentifier("contactCellCheckbox")! as! ContactCellCheckbox
         cell.textLabel?.numberOfLines = 0 //これをしないと複数表示されない
+        cell.checkbox!.selected = self.checkArray[indexPath.row]
         cell.name!.text = self.result[indexPath.row][0]
         cell.tel!.text  = self.result[indexPath.row][1]
         cell.kubun!.text = self.result[indexPath.row][3]
@@ -174,10 +206,9 @@ class ContactLoadDialog2: NSObject, UITableViewDelegate, UITableViewDataSource {
     //セルを選択
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         print("セルを選択 #\(indexPath.row)")
-        //選択されたセルを取得
-        let cell = tableView.cellForRowAtIndexPath(indexPath)
-        // チェックマークを入れる
-        cell!.accessoryType = UITableViewCellAccessoryType.Checkmark
+        //チェックを反転
+        self.checkArray[indexPath.row] = !self.checkArray[indexPath.row]
+        //状態を即刻反映するためリロードして再描画
+        table.reloadData()
     }
-
 }
